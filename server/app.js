6b7a53e7fd86c146119f86bccd59a7f453e052e1
SILGA Pinguedwindé fabrice <sliga.brice@gmail.com>
2021-06-09 19:26:09 +0200

    Added the server with ExpressJS

---
+++ server/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "../public")));
+
+mongoose.connect(
+  "mongodb+srv://angulardyma:123123123@cluster0-urpjt.gcp.mongodb.net/angulardyma?retryWrites=true&w=majority",
+  {
+    keepAlive: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  },
+  function(error) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Connexion opened to mongodb!");
+    }
+  }
+);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+module.exports = app;
